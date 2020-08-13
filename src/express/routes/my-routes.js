98c7b3f3b349f45d82a94d0ed9dde9c4bd1ea41b
Nikita Maslov <51367582+Nikmas-creator@users.noreply.github.com>
2020-08-13 15:02:14 +0300

    module3-task1 is completed

---
+++ src/express/routes/my-routes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const {Router} = require(`express`);
+const myRouter = new Router();
+
+myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+
+module.exports = myRouter;
