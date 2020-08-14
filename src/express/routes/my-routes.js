10bef056bd9867e6920bda6cd30c3b2d7ffc317d
Nikita Maslov <51367582+Nikmas-creator@users.noreply.github.com>
2020-08-14 12:15:49 +0300

    moved /my route

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,6 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
+myRouter.get(`/`, (req, res) => res.render(`tickets/my-tickets`));
 myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 
 module.exports = myRouter;
