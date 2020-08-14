9948ef421980292c7563e64e0e118996b74eb51a
Nikita Maslov <51367582+Nikmas-creator@users.noreply.github.com>
2020-08-14 12:08:45 +0300

    moved /my route

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -6,7 +6,6 @@ const mainRouter = new Router();
 mainRouter.get(`/`, (req, res) => res.send(`/`));
 mainRouter.get(`/register`, (req, res) => res.send(`/register`));
 mainRouter.get(`/login`, (req, res) => res.send(`/login`));
-mainRouter.get(`/my`, (req, res) => res.send(`/my`));
 mainRouter.get(`/search`, (req, res) => res.send(`/search`));
 
 module.exports = mainRouter;
