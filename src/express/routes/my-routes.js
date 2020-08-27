253d932cc797ba819a3b46aa9f6ec2af7960e033
Nikita Maslov <51367582+Nikmas-creator@users.noreply.github.com>
2020-08-27 20:28:06 +0300

    First 2 taskes are done

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,9 +1,49 @@
 'use strict';
 
-const {Router} = require(`express`);
+const {
+  Router
+} = require(`express`);
 const myRouter = new Router();
+const axios = require(`axios`);
+const PATH_TO_SERVICE = `http://localhost:3000`;
 
-myRouter.get(`/`, (req, res) => res.render(`tickets/my-tickets`));
-myRouter.get(`/comments`, (req, res) => res.render(`comments`));
+const getAllOffers = async () => {
+  const serviceResp = await axios.get(`${PATH_TO_SERVICE}/api/offers`);
+  return serviceResp.data;
+};
+
+const getOfferComments = async (id) => {
+  const comments = await axios.get(`${PATH_TO_SERVICE}/api/offers/${id}/comments`);
+  return comments;
+};
+
+const getFirstTwoOffersComments = async () => {
+  const firstTwoOffers = await getAllOffers().slice(0, 2);
+
+  const firstOfferID = firstTwoOffers[0];
+  const secondOfferID = firstTwoOffers[1];
+
+  const firstOfferComments = await getOfferComments(firstOfferID);
+  const secondOfferComments = await getOfferComments(secondOfferID);
+
+  return {
+    firstOfferComments,
+    secondOfferComments
+  };
+};
+
+myRouter.get(`/`, async (req, res) => {
+  const allOffers = await getAllOffers();
+  res.render(`tickets/my-tickets`, {
+    allOffers
+  });
+});
+myRouter.get(`/comments`, async (req, res) => {
+  const {firstOfferComments, secondOfferComments} = getFirstTwoOffersComments();
+  res.render(`comments`, {
+    firstOfferComments,
+    secondOfferComments
+  });
+});
 
 module.exports = myRouter;
--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,13 +1,27 @@
 'use strict';
 
-const {Router} = require(`express`);
+const {
+  Router
+} = require(`express`);
 const offersRouter = new Router();
+const axios = require(`axios`);
+const PATH_TO_SERVICE = `http://localhost:3000`;
+
+const getOffer = async (id) => {
+  const offer = await axios.get(`${PATH_TO_SERVICE}/offers/${id}`);
+  return offer;
+};
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 offersRouter.get(`/add`, (req, res) => res.render(`tickets/new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`tickets/ticket-edit`));
+
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const offerForEditing = await getOffer(req.params.id);
+  res.render(`tickets/ticket-edit`, {
+    offerForEditing
+  });
+});
+
 offersRouter.get(`/:id`, (req, res) => res.render(`tickets/ticket`));
 
 module.exports = offersRouter;
-
-
