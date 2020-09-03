3a6170f153225ee7c0c2c61447d1b171e9a84228
Nikita Maslov <maslov.n.e@gmail.com>
2020-09-03 19:19:16 +0300

    fix everything besides the multiple select

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -8,6 +8,7 @@ const fs = require(`fs`).promises;
 
 const axios = require(`axios`);
 const PATH_TO_SERVICE = `http://localhost:3000`;
+const PATH_TO_CATEGORIES = `./data/categories.txt`;
 
 const getOffer = async (id) => {
   const response = await axios.get(`${PATH_TO_SERVICE}/api/offers/${id}`);
@@ -15,13 +16,16 @@ const getOffer = async (id) => {
 };
 
 const postOffer = async (offer) => {
-  const response = await await axios.post(`${PATH_TO_SERVICE}/api/offers`, offer);
+  const response = await axios.post(`${PATH_TO_SERVICE}/api/offers`, offer);
 
   return response;
 };
 
 const normalizeOffer = ((bodyOffer) => {
-  const {fields, picture} = bodyOffer;
+  const {
+    fields,
+    picture
+  } = bodyOffer;
   return {
     title: fields[`ticket-name`],
     description: fields.comment,
@@ -32,21 +36,56 @@ const normalizeOffer = ((bodyOffer) => {
   };
 });
 
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().replace(/\r/g, ``).split(`\n`);
+  } catch (error) {
+    return [];
+  }
+};
+
+let allCategories;
+
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
-offersRouter.get(`/add`, (req, res) => res.render(`tickets/new-ticket`));
+offersRouter.get(`/add`, async (req, res) => {
+  if (!allCategories) {
+    allCategories = await readContent(PATH_TO_CATEGORIES);
+  }
+
+  res.render(`tickets/new-ticket`, {
+    fields: {allCategories}
+  });
+});
 
 offersRouter.get(`/edit/:id`, async (req, res) => {
+  if (!allCategories) {
+    allCategories = await readContent(PATH_TO_CATEGORIES);
+  }
+
   const offerForEditing = await getOffer(req.params.id);
+
   console.log(offerForEditing);
+
   res.render(`tickets/ticket-edit`, {
-    offerForEditing
+    offerForEditing,
+    fields: {allCategories}
   });
 });
 
 offersRouter.get(`/:id`, (req, res) => res.render(`tickets/ticket`));
 offersRouter.post(`/add`, async (req, res) => {
   console.log(req.fields);
-  const AVATARS_PATH = `src/express/public/img/`;
+
+  if (!allCategories) {
+    allCategories = await readContent(PATH_TO_CATEGORIES);
+  }
+
+  const allFields = Object.assign(req.fields, {
+    allCategories
+  });
+
+  const AVATARS_PATH = `src/express/public/upload/`;
   const {
     type,
     size,
@@ -58,7 +97,7 @@ offersRouter.post(`/add`, async (req, res) => {
   if (size === 0 || !allowTypes.includes(type)) {
     fs.unlink(path);
     res.render(`tickets/new-ticket`, {
-      fields: req.fields
+      fields: allFields
     });
 
     return;
@@ -71,7 +110,7 @@ offersRouter.post(`/add`, async (req, res) => {
   }
 
   const response = await postOffer(normalizeOffer({
-    fields: req.fields,
+    fields: allFields,
     picture: name
   }));
 
@@ -79,7 +118,7 @@ offersRouter.post(`/add`, async (req, res) => {
     res.redirect(`/my`);
   } else {
     res.render(`tickets/new-ticket`, {
-      fields: req.fields,
+      fields: allFields,
       picture: name
     });
   }
